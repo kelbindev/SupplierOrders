698a5cd65089d0c2e879b5b0bc18f8e80e33ec85
Kelbin <kelbin@shimano.com.sg>
2024-03-09 12:59:38 +0800

    first commit

---
+++ DependencyInjection/ConfigureRepository.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.Repository;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using Repository.Context;
+
+namespace SupplierOrders.DependencyInjection;
+
+public static class ConfigureRepository
+{
+    public static IServiceCollection ConfigureRepositoryDependecy(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.AddDbContext<SupplierOrdersContext>(opts => opts.UseSqlServer(configuration["ConnectionString:Db"]));
+        services.AddScoped<IRepositoryManager, RepositoryManager>();
+        return services;
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SupplierOrders.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
